b941208f75eba7ed525ae37df092676b1dbb1f70
karanamsirisha6 <114322132+karanamsirisha6@users.noreply.github.com>
2026-01-16 09:59:15 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,20 @@
+let operation = "*";
+switch(operation) {
+    case  "+":
+        console.log("Adition");
+        break; 
+
+    case "-":
+        console.log("subtraction");
+        break;
+
+    case "*" :
+        console.log("Multiplication");
+        break;
+        
+     default:
+        console.log("Invalid");  
+
+    
+
+}
